d86233b356cd6eedf4de6cf68ce0a8282305b104
amadeus <amadeusdias@hotmail.com>
2023-07-04 19:42:37 -0300

    dynamic routes complete

---
+++ pages/authors/me.js
@@ -0,0 +1,7 @@
+export default function Me() {
+  return <ul>
+    <li>Nome: Amadeus Ferreira Dias</li>
+    <li>Email: amadeusdias@hotmail.com</li>
+    <li><a href="https://www.linkedin.com/in/amadeus-ferreira-dias/" target="_blank">LinkedIn</a></li>
+  </ul>
+}
